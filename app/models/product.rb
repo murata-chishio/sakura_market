988162e15114f6bb67e315516a0cd7c0b9952a4d
murata-chishio <zhizhidac@gmail.com>
2025-03-21 23:52:27 +0900

    画像登録機能

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,17 @@
 class Product < ApplicationRecord
-  has_one_attached :image
+  IMAGE_SETTINGS = {
+    content_types: ['image/png', 'image/jpeg'],
+    preview_width: 500,
+    preview_height: 500,
+  }.freeze
+
+  has_one_attached :image do |attachable|
+    preview_width = IMAGE_SETTINGS[:preview_width]
+    preview_height = IMAGE_SETTINGS[:previes_height]
+    attachable.variant :preview, resize_to_limit: [preview_width, preview_height]
+  end
+
+  validates :image, content_type: IMAGE_SETTINGS[:content_types]
+
   scope :default_order, -> { order(updated_at: :desc, id: :desc) }
 end
