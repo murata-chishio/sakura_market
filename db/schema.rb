c2266857a190bedacb545b8c0f1e218eead62033
murata-chishio <zhizhidac@gmail.com>
2025-03-23 23:51:38 +0900

    ユーザーの名前と住所追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_23_114402) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_23_144400) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -88,6 +88,12 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_23_114402) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.integer "postcode"
+    t.integer "prefecture_code"
+    t.string "address_city"
+    t.string "address_street"
+    t.string "address_building"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
